09e0c162148d01d1d72207e98cb1e05de75a8153
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-09-20 17:23:18 +0900

    3

--- main.c
+++ main.c
@@ -6,13 +6,11 @@
 int main(void) 
 {
 	char c;
-	int i;
 	
-	printf("input a number:");
+	printf("input a character:");
 	scanf("%c",&c);
 	
-	i=c-'0';
-	printf("The input number is %i\n",i);
+	printf("The next character of %c(%d) is %c(%d)\n",c,c,c+1,c+1);
 	
 	return 0;
 }
